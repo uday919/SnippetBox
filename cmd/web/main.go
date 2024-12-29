44cd53316dbc5ee804704683baabfd6b05a26758
uday919 <udaykiran919p@gmail.com>
2024-12-29 11:30:32 -0600

    Handlers

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	log.Print("server started")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
